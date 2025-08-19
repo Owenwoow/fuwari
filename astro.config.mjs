2ed5c6a56c906c261c10c840852177bd6cce8f99
Owen <gonekooing@gmail.com>
2025-08-19 17:24:42 +0800

    修改指向的域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://owenwoow.com",
+	site: "https://owenwoow.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
